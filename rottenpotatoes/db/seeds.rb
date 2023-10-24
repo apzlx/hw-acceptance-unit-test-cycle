897d8f8d4047f5ec8264b7febb6e67d0d0e9a583
Lingxi Zhou <lz2648@columbia.edu>
2023-10-24 16:00:55 +0000

    fix bugs

--- rottenpotatoes/db/seeds.rb
+++ rottenpotatoes/db/seeds.rb
@@ -9,12 +9,12 @@
 movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', :director => 'test1'},
     	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984', :director => 'test2'},
     	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989', :director => 'test1'},
-      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
-      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
-      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
-      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
-      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011', :director => 'test2'},
+      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001', :director => 'test1'},
+      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001', :director => 'test1'},
+      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968', :director => 'test1'},
+      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004', :director => 'test1'},
+      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981', :director => 'test3'},
       	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
   	 ]
 
